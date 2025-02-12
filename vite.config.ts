be87b8f5ebfc4189917294c86c7182c270e845c1
Aniruddha Yeole <yeoleaniruddha2006@gmail.com>
2025-02-12 15:12:09 +0530

    Updated vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  base: '/Revino/', // ✅ Add this line (Repo name with slashes)
 });
