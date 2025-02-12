f1169efce8c40a1164300af51da146816140968c
Aniruddha Yeole <yeoleaniruddha2006@gmail.com>
2025-02-12 20:36:29 +0530

    Fixed vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Revino/', // ✅ Add this line (Repo name with slashes)
+  base: '/Revino/', // This should match your GitHub repo name
 });
